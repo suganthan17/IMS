13e7b0d8dc46cda921d391918bcd8684e2627ca9
suganthan <suganthanvadivel@gmail.com>
2026-02-15 19:58:55 +0530

    Remove Express 5 wildcard options fix

--- server/server.js
+++ server/server.js
@@ -31,8 +31,6 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
