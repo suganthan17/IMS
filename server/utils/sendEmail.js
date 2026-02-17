c2fd70d03a474bf19aa189528cb9790188345ddc
suganthan <suganthanvadivel@gmail.com>
2026-02-17 14:41:18 +0530

    EMAIL FIX 2

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,7 +6,9 @@ dotenv.config();
 export const sendEmail = async (to, subject, htmlContent) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
